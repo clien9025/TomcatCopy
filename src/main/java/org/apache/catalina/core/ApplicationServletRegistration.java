90a4586049440bb980fd25c7160b74dde7cc7f36
湛阳 <zhanyang@zhanyangs-MacBook-Pro.local>
2023-12-11 22:44:20 +0800

    Class-ApplicationServletRegistration-Finished: throw error finished && code run

--- src/main/java/org/apache/catalina/core/ApplicationServletRegistration.java
+++ src/main/java/org/apache/catalina/core/ApplicationServletRegistration.java
@@ -4,9 +4,7 @@ import jakarta.servlet.MultipartConfigElement;
 import jakarta.servlet.ServletRegistration;
 import jakarta.servlet.ServletSecurityElement;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author zhanyang
@@ -16,72 +14,76 @@ public class ApplicationServletRegistration implements ServletRegistration.Dynam
     // todo: 调用了
     @Override
     public void setLoadOnStartup(int loadOnStartup) {
-
+        System.out.println("public void setLoadOnStartup(int loadOnStartup) {");
+//        throw new UnsupportedOperationException();
     }
 
     @Override
     public Set<String> setServletSecurity(ServletSecurityElement constraint) {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     // todo: 调用了
     @Override
     public void setMultipartConfig(MultipartConfigElement multipartConfig) {
-
+        System.out.println("public void setMultipartConfig(MultipartConfigElement multipartConfig) {");
+//        throw new UnsupportedOperationException();
     }
 
     @Override
     public void setRunAsRole(String roleName) {
-
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public void setAsyncSupported(boolean isAsyncSupported) {
+        System.out.println("public void setAsyncSupported(boolean isAsyncSupported) {");
 //        throw new UnsupportedOperationException();
     }
 
     @Override
     public Set<String> addMapping(String... urlPatterns) {
-        return null;
+        // todo 自己实现的，用来站位
+        return new HashSet<>(Arrays.asList(urlPatterns));
     }
 
     @Override
     public Collection<String> getMappings() {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public String getRunAsRole() {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public String getName() {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public String getClassName() {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public boolean setInitParameter(String name, String value) {
-        return false;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public String getInitParameter(String name) {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Set<String> setInitParameters(Map<String, String> initParameters) {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Map<String, String> getInitParameters() {
-        return null;
+        throw new UnsupportedOperationException();
     }
 }
