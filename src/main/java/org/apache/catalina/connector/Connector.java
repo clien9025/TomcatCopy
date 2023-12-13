e9979ec499640134b09fa1156fef5a05213a9eeb
湛阳 <zhanyang@zhanyangs-MacBook-Pro.local>
2023-12-13 11:23:13 +0800

    Interface-Finished: Engine,Executor && Class-Finished: Class-Finished: Connector,Mapper

---
+++ src/main/java/org/apache/catalina/connector/Connector.java
@@ -0,0 +1,92 @@
+package org.apache.catalina.connector;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.LifecycleListener;
+import org.apache.catalina.LifecycleState;
+import org.apache.catalina.util.LifecycleMBeanBase;
+
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+
+public class Connector extends LifecycleMBeanBase {
+    @Override
+    public String getDomain() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setDomain(String domain) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ObjectName getObjectName() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ObjectName preRegister(MBeanServer server, ObjectName name) throws Exception {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void postRegister(Boolean registrationDone) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void preDeregister() throws Exception {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void postDeregister() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addLifecycleListener(LifecycleListener listener) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public LifecycleListener[] findLifecycleListeners() {
+//        return new LifecycleListener[0];
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void removeLifecycleListener(LifecycleListener listener) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void init() throws LifecycleException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void start() throws LifecycleException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void stop() throws LifecycleException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void destroy() throws LifecycleException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public LifecycleState getState() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getStateName() {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/main/java/org/apache/catalina/mapper/Mapper.java
@@ -0,0 +1,4 @@
+package org.apache.catalina.mapper;
+
+public final class Mapper {
+}
