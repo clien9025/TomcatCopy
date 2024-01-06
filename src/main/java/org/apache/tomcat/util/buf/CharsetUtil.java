831852cd8f5ecb34b3d7be214a5cd542a3600386
湛阳 <zhanyang@zhanyangs-MacBook-Pro.local>
2024-01-06 22:48:44 +0800

    fix

--- src/main/java/org/apache/tomcat/util/buf/CharsetUtil.java
+++ src/main/java/org/apache/tomcat/util/buf/CharsetUtil.java
@@ -35,7 +35,7 @@ public class CharsetUtil {
         CharsetDecoder decoder = charset.newDecoder();// 这行代码创建了一个解码器，用于将字节解码为字符
         ByteBuffer inBytes = ByteBuffer.allocate(1);// 创建了一个容量为1的字节缓冲区，用于存放将要检查的字节
         CharBuffer outChars;
-        for (int i = 65; i < 128; i++) {
+        for (int i = 0; i < 128; i++) {
             // 清除和放置字节
             inBytes.clear();
             inBytes.put((byte) i);// 十进制
@@ -43,12 +43,11 @@ public class CharsetUtil {
             try {
                 // 将单个字节（inBytes）解码成 outChars(字符) 并存储在CharBuffer outChars中。这个过程是:字节--->成对应的字符
                 outChars = decoder.decode(inBytes);
-//                System.out.println("outChars.get()------------> "+outChars.get());
-//                System.out.println("-----------------------------------------------------------------------------------------------");
             } catch (CharacterCodingException e) {
                 return false;
             }
             try {
+                // 检查解码后的字符是否与期望的ASCII字符匹配
                 if (outChars.get() != i) {
                     return false;
                 }
