fed295680087f09b7ef773924fd1a2823e8d5363
湛阳 <zhanyang@zhanyangs-MacBook-Pro.local>
2024-01-26 20:53:47 +0800

    temp save

--- src/main/java/org/apache/catalina/core/StandardService.java
+++ src/main/java/org/apache/catalina/core/StandardService.java
@@ -420,65 +420,66 @@ public class StandardService extends LifecycleMBeanBase implements Service {
     @Override
     protected void stopInternal() throws LifecycleException {
 
-//        synchronized (connectorsLock) {
-//            // Initiate a graceful stop for each connector
-//            // This will only work if the bindOnInit==false which is not the
-//            // default.
-//            for (Connector connector : connectors) {
-//                connector.getProtocolHandler().closeServerSocketGraceful();
-//            }
-//
-//            // Wait for the graceful shutdown to complete
-//            long waitMillis = gracefulStopAwaitMillis;
-//            if (waitMillis > 0) {
-//                for (Connector connector : connectors) {
-//                    waitMillis = connector.getProtocolHandler().awaitConnectionsClose(waitMillis);
-//                }
-//            }
-//
-//            // Pause the connectors
-//            for (Connector connector : connectors) {
-//                connector.pause();
-//            }
-//        }
-//
-//        if (log.isInfoEnabled()) {
-//            log.info(sm.getString("standardService.stop.name", this.name));
-//        }
-//        setState(LifecycleState.STOPPING);
-//
-//        // Stop our defined Container once the Connectors are all paused
-//        if (engine != null) {
-//            synchronized (engine) {
-//                engine.stop();
-//            }
-//        }
-//
-//        // Now stop the connectors
-//        synchronized (connectorsLock) {
-//            for (Connector connector : connectors) {
-//                if (!LifecycleState.STARTED.equals(connector.getState())) {
-//                    // Connectors only need stopping if they are currently
-//                    // started. They may have failed to start or may have been
-//                    // stopped (e.g. via a JMX call)
-//                    continue;
-//                }
-//                connector.stop();
-//            }
-//        }
-//
-//        // If the Server failed to start, the mapperListener won't have been
-//        // started
-//        if (mapperListener.getState() != LifecycleState.INITIALIZED) {
-//            mapperListener.stop();
-//        }
-//
-//        synchronized (executors) {
-//            for (Executor executor : executors) {
-//                executor.stop();
-//            }
-//        }
-        throw new UnsupportedOperationException();
+        synchronized (connectorsLock) {
+            // Initiate a graceful stop for each connector
+            // This will only work if the bindOnInit==false which is not the
+            // default.
+            for (Connector connector : connectors) {
+                // closeServerSocketGraceful:优雅地关闭服务器套接字
+                connector.getProtocolHandler().closeServerSocketGraceful();
+            }
+
+            // Wait for the graceful shutdown to complete
+            // gracefulStop:优雅的停止
+            long waitMillis = gracefulStopAwaitMillis;
+            if (waitMillis > 0) {
+                for (Connector connector : connectors) {
+                    waitMillis = connector.getProtocolHandler().awaitConnectionsClose(waitMillis);
+                }
+            }
+
+            // Pause the connectors
+            for (Connector connector : connectors) {
+                connector.pause();
+            }
+        }
+
+        if (log.isInfoEnabled()) {
+            log.info(sm.getString("standardService.stop.name", this.name));
+        }
+        setState(LifecycleState.STOPPING);
+
+        // Stop our defined Container once the Connectors are all paused
+        if (engine != null) {
+            synchronized (engine) {
+                engine.stop();
+            }
+        }
+
+        // Now stop the connectors
+        synchronized (connectorsLock) {
+            for (Connector connector : connectors) {
+                if (!LifecycleState.STARTED.equals(connector.getState())) {
+                    // Connectors only need stopping if they are currently
+                    // started. They may have failed to start or may have been
+                    // stopped (e.g. via a JMX call)
+                    continue;
+                }
+                connector.stop();
+            }
+        }
+
+        // If the Server failed to start, the mapperListener won't have been
+        // started
+        if (mapperListener.getState() != LifecycleState.INITIALIZED) {
+            mapperListener.stop();
+        }
+
+        synchronized (executors) {
+            for (Executor executor : executors) {
+                executor.stop();
+            }
+        }
     }
 
 
