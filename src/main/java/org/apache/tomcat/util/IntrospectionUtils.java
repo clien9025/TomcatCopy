cedb66b72a50e5985f6eec6f1129db94f68dfbfd
zhanyang <goog1e302677599@gmail.com>
2024-03-27 16:08:39 +0800

    stemp

--- src/main/java/org/apache/tomcat/util/IntrospectionUtils.java
+++ src/main/java/org/apache/tomcat/util/IntrospectionUtils.java
@@ -399,13 +399,12 @@ public final class IntrospectionUtils {
      * @return the capitalized string
      */
     public static String capitalize(String name) {
-//        if (name == null || name.length() == 0) {
-//            return name;
-//        }
-//        char chars[] = name.toCharArray();
-//        chars[0] = Character.toUpperCase(chars[0]);
-//        return new String(chars);
-        throw new UnsupportedOperationException();
+        if (name == null || name.length() == 0) {
+            return name;
+        }
+        char chars[] = name.toCharArray();
+        chars[0] = Character.toUpperCase(chars[0]);
+        return new String(chars);
     }
 
     // -------------------- other utils --------------------
